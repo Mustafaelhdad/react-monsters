612ff06ee5e36f9af2182cd2ac0a416318e88879
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2021-11-24 14:42:00 +0200

    a slight change into the title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
+      title: 'Monsters'
     }
 
     // this.handleChange = this.handleChange.bind(this)
@@ -23,16 +24,16 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
-  handleChange = e => this.setState({ searchField: e.target.value })
+  handleChange = e => this.setState({ searchField: e.target.value, title: e.target.value })
 
   render() {
 
-    const { monsters, searchField } = this.state
+    const { monsters, searchField, title } = this.state
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
 
     return (
       <div className="App">
-        <h1>Monsters</h1>
+        <h1>{title}</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
